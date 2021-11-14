1aee6b92fb4ee9c21830e4afa6d2fd031511afc2
Sayem Chowdhury <sayem314@gmail.com>
2021-11-15 00:25:07 +0600

    use yt-dlp

--- updater.js
+++ updater.js
@@ -1,38 +1,42 @@
-const { writeFileSync } = require('fs')
-const { join } = require('path')
-const { execFileSync } = require('child_process')
-const axios = require('axios')
+const { writeFileSync } = require("fs");
+const { join } = require("path");
+const { execFileSync } = require("child_process");
+const axios = require("axios");
 
 /**
  * Updates or downloads the youtube-dl binary.
  * @return {Promise<String>} - New version number
  */
-async function updateBinary () {
-  const bin = `youtube-dl${process.platform === 'win32' ? '.exe' : ''}`
-  const dest = join(__dirname, 'ytdl', bin)
+async function updateBinary() {
+  const name = `yt-dlp${process.platform === "win32" ? ".exe" : ""}`;
+  const resp = await axios.get(
+    "https://api.github.com/repos/yt-dlp/yt-dlp/releases/latest"
+  );
+  const asset = resp.data.assets.find((a) => a.name === name);
 
-  const { data } = await axios.get(
-    `https://yt-dl.org/downloads/latest/${bin}`,
-    { responseType: 'arraybuffer' }
-  )
+  const bin = `youtube-dl${process.platform === "win32" ? ".exe" : ""}`;
+  const dest = join(__dirname, "ytdl", bin);
+  const { data } = await axios.get(asset.browser_download_url, {
+    responseType: "arraybuffer",
+  });
 
-  writeFileSync(dest, data, { mode: '755', encoding: 'binary' })
-  return execFileSync(dest, ['--version'])
+  writeFileSync(dest, data, { mode: "755", encoding: "binary" });
+  return execFileSync(dest, ["--version"]);
 }
 
 if (require.main === module) {
   // CLI
-  console.log('Updating youtube-dl to the latest version...')
+  console.log("Updating youtube-dl to the latest version...");
   updateBinary()
     .then((version) => {
-      console.log(`Successfully downloaded youtube-dl ${version}`)
-      process.exit()
+      console.log(`Successfully downloaded youtube-dl ${version}`);
+      process.exit();
     })
     .catch((e) => {
-      console.error(e)
-      process.exit(1)
-    })
+      console.error(e);
+      process.exit(1);
+    });
 } else {
   // Module
-  module.exports = updateBinary
+  module.exports = updateBinary;
 }
